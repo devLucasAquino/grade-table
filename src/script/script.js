f7869187b16be46a2704c1862c6a98f7b9f7a5db
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-02-15 21:28:37 -0300

    fix function avarageStudentsList

--- src/script/script.js
+++ src/script/script.js
@@ -1,19 +1,19 @@
 function avarageStudentsList(){
     let table = document.getElementById("table");
     let numberOfRows = table.rows.length;
-    let avarageStudents = '';
+    let avarageStudents = [];
 
     for(var i = 1; i < numberOfRows; i++){
-        let avarageStudent = 0;
+        let gradeStudent = 0;
 
         for(var j = 2; j < 6; j++){
             let celulaInput = table.rows[i].cells[j].querySelector('input');
             let nota = celulaInput ? celulaInput.value : undefined;
 
-            avarageStudent += Number(nota);
+            gradeStudent += Number(nota);
         }
 
-       avarageStudents += avarageStudent/4; 
+       avarageStudents.push(gradeStudent/4); 
     }
 
     return avarageStudents;
